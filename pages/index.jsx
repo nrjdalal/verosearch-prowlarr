33ece0c1af8d528de780f785d937e57eab8ae32a
Neeraj Dalal <admin@nrjdalal.com>
2022-08-17 21:27:18 +0530

    Wed Aug 17 21:27:18 IST 2022

--- pages/index.jsx
+++ pages/index.jsx
@@ -36,16 +36,27 @@ const Home = () => {
       <main className="my-8 flex items-center justify-between">
         <input
           type="text"
-          className="h-10 w-full max-w-3xl rounded-lg border-2 text-xl"
+          className="h-10 w-full max-w-3xl rounded-lg border-2 border-gray-300 text-xl"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
 
         <button
-          className={`ml-4 h-10 rounded-lg bg-black px-8 text-xl text-white ${status ? 'animate-pulse' : ''}`}
+          className={`ml-4 flex h-10 w-10 items-center justify-center rounded-lg bg-black text-xl text-white ${
+            status ? 'animate-pulse' : ''
+          }`}
           onClick={Searcher}
         >
-          {status ? 'Searching' : 'Search'}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+          </svg>
         </button>
       </main>
 
