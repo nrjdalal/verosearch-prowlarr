1c1ab5052d6ce519485785e9d9716d142d3b0977
Neeraj Dalal <admin@nrjdalal.com>
2022-08-17 20:15:59 +0530

    Wed Aug 17 20:15:59 IST 2022

--- pages/index.jsx
+++ pages/index.jsx
@@ -43,9 +43,27 @@ const Home = () => {
       <div className="mt-4 flex flex-col gap-y-2 pb-8">
         {results.map((element, key) => {
           return (
-            <p className="text-black" key={key}>
-              {key + 1} <br /> {element.title}
-            </p>
+            <div className="flex" key={key}>
+              <p className="text-black">
+                {key + 1} {element.title}
+              </p>
+              <a href={element.link}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M9 19l3 3m0 0l3-3m-3 3V10"
+                  />
+                </svg>
+              </a>
+            </div>
           )
         })}
       </div>
