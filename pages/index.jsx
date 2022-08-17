80753e68ae7c6a1b69ba35e01c234a78f0ab9694
Neeraj Dalal <admin@nrjdalal.com>
2022-08-17 18:12:19 +0530

    Wed Aug 17 18:12:19 IST 2022

---
+++ pages/index.jsx
@@ -0,0 +1,54 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
+
+const Home = () => {
+  const [search, setSearch] = useState([])
+
+  useEffect(() => {}, [search])
+
+  const Searcher = async () => {
+    const xml = await fetch(
+      'https://jackett.at7.in/api/v2.0/indexers/cloudtorrents/results/torznab?apikey=qbittorrent&q=the+boys'
+    )
+    const res = await await xml.text()
+
+    let items = res.split('<item>')
+
+    const array = []
+
+    items.forEach((element) => {
+      if (element.includes('<size>')) {
+        const title = element.split('<title>')[1].split('</title>')[0]
+
+        array.push({ title: title })
+      }
+    })
+
+    setSearch(array)
+  }
+
+  return (
+    <div>
+      <main className="flex w-full items-center justify-center gap-x-8 px-20 py-10 text-center">
+        <button className="rounded-xl bg-black p-8 py-2 text-xl text-white" onClick={Searcher}>
+          Search
+        </button>
+
+        <button onClick={() => setSearch([])}>Clear</button>
+      </main>
+
+      <div className="pb-8">
+        {search.map((element, key) => {
+          return (
+            <p className="text-center text-black" key={key}>
+              {key} ~ {element.title}
+            </p>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Home
