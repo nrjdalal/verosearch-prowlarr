c78c0049ba11073f7e380d83a5744f97dc56e0aa
Neeraj Dalal <admin@nrjdalal.com>
2022-08-18 22:01:05 +0530

    Thu Aug 18 22:01:05 IST 2022

--- pages/index.jsx
+++ pages/index.jsx
@@ -167,7 +167,7 @@ const Home = () => {
               }
               <p className="break-all font-medium text-black line-clamp-2">{element.title}</p>
 
-              <div className="mt-2 flex items-center justify-between">
+              <div className="mt-2 flex items-center justify-between text-slate-500">
                 <div className="flex items-center gap-2 text-xs md:text-sm">
                   {
                     // ~ Time
