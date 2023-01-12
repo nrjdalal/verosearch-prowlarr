e8e58ccc0d7c30eba89f30ce8e1bc8409ed8a268
Neeraj Dalal <admin@nrjdalal.com>
2023-01-12 15:12:22 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    APIURL: process.env.APIURL || 'https://prowlarr.at7.in',
-    APIKEY: process.env.APIKEY || '3fbb3827a1c34737bf268287c4c93986',
+    APIURL: process.env.APIURL || 'https://prowlarr.at7.in', // Your Prowlarr hosting URL/Domain e.g. https://prowlarr.com
+    APIKEY: process.env.APIKEY || '3fbb3827a1c34737bf268287c4c93986', // Prowlarr > Settings > General > Security > API Key
   },
 }
 
